927b61d553adf2724655fb1b785230c295cf5974
Erdij <yildirimsukruerdi@gmail.com>
2022-11-15 17:26:16 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import "./App.css";
 
 function App() {
+  const [timer, setTimer] = useState(5);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(timer - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timer]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          height: "250px",
+          width: "250px",
+          backgroundColor: "blue",
+          borderRadius: "50%",
+          color: "white",
+          fontSize: "60px",
+          margin: "10px auto",
+          lineHeight: "250px",
+        }}
+      >
+        {timer}
+      </div>
     </div>
   );
 }
